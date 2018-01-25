05fb7c952e06b30d770998ace6dd7b5d478f9bc7
Zeeshan Ahmad <ziishaned@gmail.com>
2018-01-25 11:53:21 +0400

    Update

--- tests/Helper/PasswordTest.php
+++ tests/Helper/PasswordTest.php
@@ -7,27 +7,25 @@ use Tests\Base\BaseCase;
 
 class PasswordTest extends BaseCase
 {
-
     public function testPassword()
     {
         $data = [
-            'plain' => '',
-            'hashed' => ''
+            'plain'  => '',
+            'hashed' => '',
         ];
         $hash = Password::hash($data['plain']);
         // return null if no password given
         $this->assertEquals(null, $hash);
 
         $data = [
-            'plain' => 'testpassword',
-            'hashed' => '$2y$10$mDjRk9dNMHiGT1WH/ZtoGOGblNIDO37pLByaxjkm6E/gLaaxr1tPm'
+            'plain'  => 'testpassword',
+            'hashed' => '$2y$10$mDjRk9dNMHiGT1WH/ZtoGOGblNIDO37pLByaxjkm6E/gLaaxr1tPm',
         ];
         $hash = Password::hash($data['plain']);
 
         $this->assertEquals(true, Password::verify($data['plain'], $hash));
 
-        // test only hsah password if not hashed before
-        $hash = Password::hash($hash);
+        // test only hash password if not hashed before
         $hash = Password::hash($hash);
         $this->assertEquals(true, Password::verify($data['plain'], $hash));
     }
