5486c2a9652e61f35749898d047926c6efecd477
Zeeshan Ahmad <ziishaned@gmail.com>
2018-02-19 18:57:18 +0400

    Replce dist with override ini

--- config/ini.settings.php
+++ config/ini.settings.php
@@ -1,25 +1,31 @@
 <?php
 
+use Ini\Parser;
+
 define('ROOT_DIR', __DIR__ . '/..');
 
-if(file_exists(ROOT_DIR . '/.config.ini')) {
-    $baseConfigFile = file_get_contents(ROOT_DIR . '/.config.ini');
+$overrideConfigFile = ROOT_DIR . '/.config.override.ini';
+$baseConfigFile = ROOT_DIR . '/.config.ini';
 
-    $parser = new \Ini\Parser();
+// Load the original configuration
+$originalConfig = file_get_contents($baseConfigFile);
 
-    $parser->use_array_object        = false;
-    $parser->ini_parse_option        = INI_SCANNER_TYPED;
-    $parser->array_literals_behavior = \Ini\Parser::PARSE_JSON;
-    $parser->treat_ini_string        = true;
-
-    $parsedConfig = $parser->parse($baseConfigFile);
-
-    $env = $parsedConfig['ENV'] ?? 'local';
-    if (!isset($parsedConfig[$env])) {
-        throw new RuntimeException("Configuration group `${env}` missing in the configuration file");
-    }
-
-    return $parsedConfig[$env];
+// Append the override if available
+if (file_exists($overrideConfigFile)) {
+    $originalConfig = $originalConfig . PHP_EOL . file_get_contents($overrideConfigFile);
 }
 
-return [];
+$parser                          = new Parser();
+$parser->use_array_object        = false;
+$parser->ini_parse_option        = INI_SCANNER_TYPED;
+$parser->array_literals_behavior = Parser::PARSE_JSON;
+$parser->treat_ini_string        = true;
+
+$parsedConfig = $parser->parse($originalConfig);
+
+$env = $parsedConfig['ENV'] ?? 'local';
+if (!isset($parsedConfig[$env])) {
+    throw new RuntimeException("Configuration group `${env}` missing in the configuration file");
+}
+
+return $parsedConfig[$env];
