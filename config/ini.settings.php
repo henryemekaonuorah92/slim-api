0489743e12cf269d9e334a535e65397bb3d86e1f
Zeeshan Ahmad <ziishaned@gmail.com>
2018-02-19 15:36:23 +0400

    Return empty array if there is no config file

--- config/ini.settings.php
+++ config/ini.settings.php
@@ -2,20 +2,24 @@
 
 define('ROOT_DIR', __DIR__ . '/..');
 
-$baseConfigFile = file_get_contents(ROOT_DIR . '/.config.ini');
+if(file_exists(ROOT_DIR . '/.config.ini')) {
+    $baseConfigFile = file_get_contents(ROOT_DIR . '/.config.ini');
 
-$parser = new \Ini\Parser();
+    $parser = new \Ini\Parser();
 
-$parser->use_array_object        = false;
-$parser->ini_parse_option        = INI_SCANNER_TYPED;
-$parser->array_literals_behavior = \Ini\Parser::PARSE_JSON;
-$parser->treat_ini_string        = true;
+    $parser->use_array_object        = false;
+    $parser->ini_parse_option        = INI_SCANNER_TYPED;
+    $parser->array_literals_behavior = \Ini\Parser::PARSE_JSON;
+    $parser->treat_ini_string        = true;
 
-$parsedConfig = $parser->parse($baseConfigFile);
+    $parsedConfig = $parser->parse($baseConfigFile);
 
-$env = $parsedConfig['ENV'] ?? 'local';
-if (!isset($parsedConfig[$env])) {
-    throw new RuntimeException("Configuration group `${env}` missing in the configuration file");
+    $env = $parsedConfig['ENV'] ?? 'local';
+    if (!isset($parsedConfig[$env])) {
+        throw new RuntimeException("Configuration group `${env}` missing in the configuration file");
+    }
+
+    return $parsedConfig[$env];
 }
 
-return $parsedConfig[$env];
+return [];
