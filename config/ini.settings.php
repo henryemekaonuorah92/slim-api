598ac2cb5d8739cf7bd5b727650272556c3e6c7d
Zeeshan Ahmad <ziishaned@gmail.com>
2018-02-19 16:00:39 +0400

    Use early exit

--- config/ini.settings.php
+++ config/ini.settings.php
@@ -2,24 +2,24 @@
 
 define('ROOT_DIR', __DIR__ . '/..');
 
-if(file_exists(ROOT_DIR . '/.config.ini')) {
-    $baseConfigFile = file_get_contents(ROOT_DIR . '/.config.ini');
-
-    $parser = new \Ini\Parser();
-
-    $parser->use_array_object        = false;
-    $parser->ini_parse_option        = INI_SCANNER_TYPED;
-    $parser->array_literals_behavior = \Ini\Parser::PARSE_JSON;
-    $parser->treat_ini_string        = true;
-
-    $parsedConfig = $parser->parse($baseConfigFile);
-
-    $env = $parsedConfig['ENV'] ?? 'local';
-    if (!isset($parsedConfig[$env])) {
-        throw new RuntimeException("Configuration group `${env}` missing in the configuration file");
-    }
-
-    return $parsedConfig[$env];
+if(!file_exists(ROOT_DIR . '/.config.ini')) {
+    return [];
 }
 
-return [];
+$baseConfigFile = file_get_contents(ROOT_DIR . '/.config.ini');
+
+$parser = new \Ini\Parser();
+
+$parser->use_array_object        = false;
+$parser->ini_parse_option        = INI_SCANNER_TYPED;
+$parser->array_literals_behavior = \Ini\Parser::PARSE_JSON;
+$parser->treat_ini_string        = true;
+
+$parsedConfig = $parser->parse($baseConfigFile);
+
+$env = $parsedConfig['ENV'] ?? 'local';
+if (!isset($parsedConfig[$env])) {
+    throw new RuntimeException("Configuration group `${env}` missing in the configuration file");
+}
+
+return $parsedConfig[$env];
