43259a528bbee79c41ba03bb39c30f6a8e28eb67
Mohamed Meabed <Meabed@users.noreply.github.com>
2018-02-19 23:09:17 +0400

    Update ini.settings.php

--- config/ini.settings.php
+++ config/ini.settings.php
@@ -4,7 +4,6 @@ use Ini\Parser;
 
 define('ROOT_DIR', __DIR__ . '/..');
 
-<<<<<<< HEAD
 $overrideConfigFile = ROOT_DIR . '/.config.override.ini';
 $baseConfigFile = ROOT_DIR . '/.config.ini';
 
@@ -23,22 +22,6 @@ $parser->array_literals_behavior = Parser::PARSE_JSON;
 $parser->treat_ini_string        = true;
 
 $parsedConfig = $parser->parse($originalConfig);
-=======
-if(!file_exists(ROOT_DIR . '/.config.ini')) {
-    return [];
-}
-
-$baseConfigFile = file_get_contents(ROOT_DIR . '/.config.ini');
-
-$parser = new \Ini\Parser();
-
-$parser->use_array_object        = false;
-$parser->ini_parse_option        = INI_SCANNER_TYPED;
-$parser->array_literals_behavior = \Ini\Parser::PARSE_JSON;
-$parser->treat_ini_string        = true;
-
-$parsedConfig = $parser->parse($baseConfigFile);
->>>>>>> 598ac2cb5d8739cf7bd5b727650272556c3e6c7d
 
 $env = $parsedConfig['ENV'] ?? 'local';
 if (!isset($parsedConfig[$env])) {
